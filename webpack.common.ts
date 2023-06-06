92f54da159c492efd27f40d2856a0b114fbb02f4
kaiyin <45481560+cherishs001@users.noreply.github.com>
2023-06-06 17:23:29 +0800

    style: add color, elevation, font config

--- webpack.common.ts
+++ webpack.common.ts
@@ -1,4 +1,5 @@
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import * as path from 'path';
 import * as webpack from 'webpack';
 
@@ -30,12 +31,30 @@ const config: webpack.Configuration = {
           },
         },
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // 提取到单独的CSS文件
+          MiniCssExtractPlugin.loader,
+          // 转换 CSS 到 CommonJS
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true, //资源映射
+            },
+          },
+          // 给 CSS 添加前缀以适应各浏览器
+          'postcss-loader',
+          // 编译 Sass 到 CSS
+          'sass-loader',
+        ],
+      },
     ],
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [new CleanWebpackPlugin(), new MiniCssExtractPlugin()],
 };
 
 export default config;
