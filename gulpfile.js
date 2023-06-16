4a95763ef45ebaef5d28a5f1b28a3d362cb34fe7
kaiyin <45481560+cherishs001@users.noreply.github.com>
2023-06-16 10:30:46 +0800

    build: delete sass config

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,5 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
-const sass = require('gulp-sass')(require('sass'));
-const autoprefixer = require('gulp-autoprefixer');
-const cssnano = require('gulp-cssnano');
-const through2 = require('through2');
 
 const paths = {
   dest: {
@@ -26,36 +22,7 @@ function compileScripts(babelEnv, destDir) {
   return gulp
     .src(scripts)
     .pipe(babel()) // 使用gulp-babel处理
-    .pipe(
-      through2.obj(function z(file, encoding, next) {
-        this.push(file.clone());
-        // 找到目标
-        if (file.path.match(/(\/|\\)style(\/|\\)index\.js/)) {
-          const content = file.contents.toString(encoding);
-          file.contents = Buffer.from(cssInjection(content)); // 文件内容处理
-          file.path = file.path.replace(/index\.js/, 'css.js'); // 文件重命名
-          this.push(file); // 新增该文件
-          next();
-        } else {
-          next();
-        }
-      })
-    )
     .pipe(gulp.dest(destDir));
-}
-
-/**
- * 当前组件样式 import './index.scss' => import './index.css'
- * 依赖的其他组件样式 import '../test-comp/style' => import '../test-comp/style/css.js'
- * 依赖的其他组件样式 import '../test-comp/style/index.js' => import '../test-comp/style/css.js'
- * @param {string} content
- */
-function cssInjection(content) {
-  console.log(content);
-  return content
-    .replace(/\/style\/?'/g, "/style/css'")
-    .replace(/\/style\/?"/g, '/style/css"')
-    .replace(/\.scss/g, '.css');
 }
 
 /**
@@ -74,30 +41,10 @@ function compileESM() {
   return compileScripts('esm', dest.esm);
 }
 
-/**
- * 拷贝less文件
- */
-function copySass() {
-  return gulp.src(paths.styles).pipe(gulp.dest(paths.dest.lib)).pipe(gulp.dest(paths.dest.esm));
-}
-
-/**
- * 生成css文件
- */
-function sass2css() {
-  return gulp
-    .src(paths.styles)
-    .pipe(sass({ outputStyle: 'compressed' })) // 处理less文件
-    .pipe(autoprefixer()) // 根据browserslistrc增加前缀
-    .pipe(cssnano({ zindex: false, reduceIdents: false })) // 压缩
-    .pipe(gulp.dest(paths.dest.lib))
-    .pipe(gulp.dest(paths.dest.esm));
-}
-
 const buildScripts = gulp.series(compileCJS, compileESM);
 
 // 并行任务 后续加入样式处理 可以并行处理
-const build = gulp.parallel(buildScripts, copySass, sass2css);
+const build = gulp.parallel(buildScripts);
 
 exports.build = build;
 
