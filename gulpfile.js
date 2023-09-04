0651c558ccc690d9343ac2781f682f6f2aaf8154
cherish <45481560+cherishs001@users.noreply.github.com>
2023-09-04 14:32:47 +0800

    feat: export css variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const webpack = require('webpack');
+const gulpWebpack = require('webpack-stream');
+const through2 = require('through2');
+const fs = require('fs');
 
 const paths = {
   dest: {
-    lib: 'lib', // commonjs 文件存放的目录名 - 本块关注
-    esm: 'esm', // ES module 文件存放的目录名 - 暂时不关心
-    dist: 'dist', // umd文件存放的目录名 - 暂时不关心
+    lib: 'lib', // commonjs
+    esm: 'esm', // ES module
+    dist: 'dist', // umd
   },
   scripts: ['src/**/*.{ts,tsx}', '!src/**/demo/*.{ts,tsx}'], // 脚本文件路径
 };
@@ -40,7 +44,63 @@ function compileESM() {
   return compileScripts('esm', dest.esm);
 }
 
-const buildScripts = gulp.series(compileCJS, compileESM);
+/**
+ * 编译umd
+ */
+function compileUMD() {
+  return gulp
+    .src(`${paths.dest.esm}/index.js`)
+    .pipe(
+      gulpWebpack(
+        {
+          output: {
+            filename: 'tetrisly.production.min.js',
+            library: {
+              type: 'umd',
+              name: 'tetrislyJS',
+            },
+          },
+          mode: 'production',
+        },
+        webpack,
+      ),
+    )
+    .pipe(gulp.dest(paths.dest.dist));
+}
+
+/**
+ * 生成css
+ */
+function generateCssVar() {
+  return gulp.src(`${paths.dest.lib}/styles/color.js`).pipe(
+    through2({ objectMode: true }, function (file, encoding, next) {
+      const { ColorConfig } = require(file.path);
+
+      let cssContent = ':root {\n';
+      let sassContent = '// color config';
+      let lessContent = '// color config';
+      for (const varName in ColorConfig) {
+        const cssVarName = `  --color-${varName.replace(/([A-Z])/g, '-$1').toLowerCase()}`;
+        const sassVarName = `$color${varName}`;
+        const lessVarName = `@color${varName}`;
+        cssContent += `${cssVarName}: ${ColorConfig[varName]};\n`;
+        sassContent += `${sassVarName}: ${ColorConfig[varName]};\n`;
+        lessContent += `${lessVarName}: ${ColorConfig[varName]};\n`;
+      }
+      cssContent += '}';
+      fs.writeFileSync(`${paths.dest.lib}/styles/color.css`, cssContent);
+      fs.writeFileSync(`${paths.dest.esm}/styles/color.css`, cssContent);
+      fs.writeFileSync(`${paths.dest.lib}/styles/color.scss`, sassContent);
+      fs.writeFileSync(`${paths.dest.esm}/styles/color.scss`, sassContent);
+      fs.writeFileSync(`${paths.dest.lib}/styles/color.less`, lessContent);
+      fs.writeFileSync(`${paths.dest.esm}/styles/color.less`, lessContent);
+
+      next(null, file);
+    }),
+  );
+}
+
+const buildScripts = gulp.series(compileCJS, compileESM, compileUMD, generateCssVar);
 
 // 并行任务 后续加入样式处理 可以并行处理
 const build = gulp.parallel(buildScripts);
