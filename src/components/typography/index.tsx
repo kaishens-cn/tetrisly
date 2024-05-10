6281c7969f35836083c52017c29b0c051d6b7176
cherish <45481560+cherishs001@users.noreply.github.com>
2024-05-10 16:27:02 +0800

    typography add user-select

--- src/components/typography/index.tsx
+++ src/components/typography/index.tsx
@@ -1,12 +1,15 @@
+import { Property } from 'csstype';
 import React, { FC } from 'react';
 
 import { ColorConfig } from '../../';
 import { Font } from '../../styles/typography';
+import UserSelect = Property.UserSelect;
 
 export interface TypographyProps extends React.HTMLAttributes<HTMLSpanElement> {
   size?: 25 | 50 | 75 | 100 | 200 | 300 | 400 | 500 | 600;
   underline?: boolean;
   color?: string;
+  userSelect?: UserSelect;
 }
 
 const withTypography = (type: 'strong' | 'medium' | 'normal'): FC<TypographyProps> => {
@@ -18,6 +21,7 @@ const withTypography = (type: 'strong' | 'medium' | 'normal'): FC<TypographyProp
           color: color,
           textDecorationLine: underline ? 'underline' : 'unset',
           cursor: underline ? 'pointer' : 'unset',
+          userSelect: props.userSelect || 'auto',
           ...style,
         }}
         weight={type}
